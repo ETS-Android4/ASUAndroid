c8b78b2eecdd1c4d1e877b6259b5db2a645ba381
Sean Knight <sceneknight@gmail.com>
2020-09-06 10:50:08 -0500

    JavaFragment.commit logic

--- app/src/main/java/com/example/asuandroid/HomeScreen.java
+++ app/src/main/java/com/example/asuandroid/HomeScreen.java
@@ -1,20 +1,35 @@
 package com.example.asuandroid;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.fragment.app.Fragment;
-
+import androidx.navigation.Navigation;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 
-public class HomeScreen extends Fragment {
-    
+
+public class HomeScreen extends Fragment implements View.OnClickListener {
+    Button btn_gotoFragment2;
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+    }
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home_screen, container, false);
+        View view = inflater.inflate(R.layout.fragment_home_screen, container, false);
+
+        btn_gotoFragment2 = getView().findViewById(R.id.action_FirstFragment_to_SecondFragment);
+        btn_gotoFragment2.setOnClickListener(this);
+
+        return view;
+    }
+    @Override
+    public void onClick(View view) {
+        Navigation.findNavController(view).navigate(R.id.action_FirstFragment_to_SecondFragment);
     }
 }
--- app/src/main/java/com/example/asuandroid/MainActivity.java
+++ app/src/main/java/com/example/asuandroid/MainActivity.java
@@ -10,52 +10,19 @@ import android.widget.Toast;
 import android.widget.AdapterView.OnItemSelectedListener;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 
 public class MainActivity extends AppCompatActivity {
-
-    Spinner spinner;
-
-    HomeScreen HomeScreen;
-    NewUniformPrompt NewUniformPrompt;
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        spinner = findViewById(R.id.rankSpinner);
-
-        HomeScreen = new HomeScreen();
-        NewUniformPrompt = new NewUniformPrompt();
-
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(MainActivity.this,
-                R.layout.fragment_new_uniform_prompt);
-                getResources().getStringArray(R.array.fragments);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
-        spinner.setAdapter(adapter);
-
-        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-                switch(i){
-                    case 0:
-                        break;
-                    case 1:
-                        break;
-                }
-            }
-            public void onNothingSelected(AdapterView<?> adapterView) {
-
-            }
-        });
+        HomeScreen fragment1 = new HomeScreen();
+        getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment1).commit();
+        NewUniformPrompt NewUniformPrompt = new NewUniformPrompt();
     }
-    public void setFragment(Fragment fragment){
-        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
-        fragmentTransaction.replace(R.id.main_frame, fragment);
-        fragmentTransaction.commit();
-    }
-
 }
