03b22a08580b820c5f3acd7d2e6be2aa6c72a098
Sean Knight <sceneknight@gmail.com>
2020-10-02 16:41:40 -0500

    zoomageView implementation

--- app/src/main/java/com/example/asuandroid/screens/UniformPresentationFragment.java
+++ app/src/main/java/com/example/asuandroid/screens/UniformPresentationFragment.java
@@ -1,23 +1,35 @@
 package com.example.asuandroid.screens;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+
+import androidx.appcompat.widget.SwitchCompat;
 import androidx.fragment.app.Fragment;
 import androidx.annotation.NonNull;
 import androidx.navigation.fragment.NavHostFragment;
 
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CompoundButton;
 
 import com.example.asuandroid.R;
 
+public class UniformPresentationFragment extends Fragment{
 
-public class UniformPresentationFragment extends Fragment {
-
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_uniform_presentation, container, false);
+    }
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
-        //SubsamplingScaleImageView imageView = (SubsamplingScaleImageView)findViewById(id.imageView);
-        //imageView.setImage(ImageSource.resource(R.drawable.pylon));
         super.onViewCreated(view, savedInstanceState);
-        view.findViewById(R.id.btn_return_home).setOnClickListener(new View.OnClickListener() {
-            @Override
+        view.findViewById(R.id.btn_return_home_fromPresentation).setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
                 NavHostFragment.findNavController(UniformPresentationFragment.this)
                         .navigate(R.id.action_uniformPresentationFragment_to_HomeScreen);
