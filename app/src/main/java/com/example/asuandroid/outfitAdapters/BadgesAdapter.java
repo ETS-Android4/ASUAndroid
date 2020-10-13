c058c633eb625494b7b66dff3c4142b52c4d0917
Sean Knight <sceneknight@gmail.com>
2020-10-12 19:11:12 -0500

    CardUpdateForRibbon

---
+++ app/src/main/java/com/example/asuandroid/outfitAdapters/BadgesAdapter.java
@@ -0,0 +1,52 @@
+package com.example.asuandroid.outfitAdapters;
+
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.view.View;
+import com.example.asuandroid.R;
+import com.example.asuandroid.outfitfragments.AwardItem;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+
+public class BadgesAdapter extends RecyclerView.Adapter<BadgesAdapter.BadgeViewHolder> {
+    private ArrayList<AwardItem> mBadgeList;
+
+    public static class BadgeViewHolder extends RecyclerView.ViewHolder {
+        public ImageView mImageView;
+        public TextView mTextView1;
+        public TextView mTextView2;
+        public BadgeViewHolder(View itemView) {
+            super(itemView);
+            mImageView = itemView.findViewById(R.id.imageView);
+            mTextView1 = itemView.findViewById(R.id.textView);
+            mTextView2 = itemView.findViewById(R.id.textView2);
+        }
+    }
+    public void BadgeAdapter(ArrayList<AwardItem> badgeList) {
+        mBadgeList = badgeList;
+    }
+    @NotNull
+    @Override
+    public BadgeViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.award_item, parent, false);
+        BadgeViewHolder evh = new BadgeViewHolder(v);
+        return evh;
+    }
+    @Override
+    public void onBindViewHolder(BadgeViewHolder holder, int position) {
+        AwardItem currentItem = (AwardItem) mBadgeList.get(position);
+        holder.mImageView.setImageResource(currentItem.getImageResource());
+        holder.mTextView1.setText(currentItem.getText1());
+        holder.mTextView2.setText(currentItem.getText2());
+    }
+    @Override
+    public int getItemCount() {
+        return mBadgeList.size();
+    }
+}
