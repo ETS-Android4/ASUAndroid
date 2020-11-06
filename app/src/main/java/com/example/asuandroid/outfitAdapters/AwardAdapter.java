cf774c9988a276c3ce7457b9eb1e10388b36254a
Sean Knight <sceneknight@gmail.com>
2020-11-06 12:45:58 -0600

    cleanFix

--- app/src/main/java/com/example/asuandroid/outfitAdapters/AwardAdapter.java
+++ app/src/main/java/com/example/asuandroid/outfitAdapters/AwardAdapter.java
@@ -1,6 +1,7 @@
 package com.example.asuandroid.outfitAdapters;
 
 import android.annotation.SuppressLint;
+import android.graphics.Color;
 import android.graphics.drawable.AnimationDrawable;
 import android.media.Image;
 import android.view.LayoutInflater;
@@ -47,13 +48,14 @@ public class AwardAdapter extends RecyclerView.Adapter<AwardAdapter.AwardViewHol
             mTextView1 = itemView.findViewById(R.id.textView);
             mTextView2 = itemView.findViewById(R.id.textView2);
             mRibbonAdd = itemView.findViewById(R.id.img_addRibbon);
-            mRibbonAdd.setOnClickListener(new View.OnClickListener() {
+            itemView.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
                     if (!isRibbonOn) {
                         System.out.println(isRibbonOn);
                         isRibbonOn = true;
                         System.out.println(isRibbonOn);
+                        itemView.setBackgroundColor(Color.GRAY);
                         ImageView rocketImage = (ImageView) v.findViewById(R.id.img_addRibbon);
                         rocketImage.setBackgroundResource(R.drawable.ribbon_anim);
                         AnimationDrawable rocketAnimation = (AnimationDrawable) rocketImage.getBackground();
@@ -62,6 +64,7 @@ public class AwardAdapter extends RecyclerView.Adapter<AwardAdapter.AwardViewHol
                         System.out.println(isRibbonOn);
                         isRibbonOn = false;
                         System.out.println(isRibbonOn);
+                        itemView.setBackgroundColor(Color.WHITE);
                         ImageView rocketImage = (ImageView) v.findViewById(R.id.img_addRibbon);
                         rocketImage.setBackgroundResource(R.drawable.ribbon_anim_return);
                         AnimationDrawable rocketAnimation = (AnimationDrawable) rocketImage.getBackground();
@@ -89,7 +92,7 @@ public class AwardAdapter extends RecyclerView.Adapter<AwardAdapter.AwardViewHol
     public AwardViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.award_item, parent, false);
         AwardViewHolder evh = new AwardViewHolder(v, mListener);
-        //evh.setIsRecyclable(false);
+        //evh.setIsRecyclable(false); BAD
         return evh;
     }
     @Override
@@ -99,20 +102,18 @@ public class AwardAdapter extends RecyclerView.Adapter<AwardAdapter.AwardViewHol
         holder.mTextView1.setText(currentItem.getText1());
         holder.mTextView2.setText(currentItem.getText2());
         //holder.mRibbonSwitch.setChecked(currentItem.getSwitch1(position).isChecked());
+
     }
     @Override
     public int getItemCount() {
         return mAwardList.size();
     }
-
     @Override
     public int getItemViewType(int position) {
         return position;
     }
-
     @Override
     public long getItemId(int position) {
         return position;
     }
-
 }
